692f62f1c6a7016199e89ccb8b30cb0ef8803f27
Antoni Kaniowski <a@generativestuff.com>
2016-03-27 17:56:45 +0200

    index shows all known channels

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var Slack = require('slack-node');
 
 /* GET home page. */
+
+slack = new Slack(process.env.SLACK_API_KEY);
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Slack to RSS by @gozmike', address: req.headers.host });
+	slack.api('channels.list', function(err, response) {
+		var channels = response.channels;
+  	res.render('index', {
+  	  	title: '',
+  	  	address: req.headers.host,
+  	  	channels: channels
+  	});
+	})
 });
 
 module.exports = router;
